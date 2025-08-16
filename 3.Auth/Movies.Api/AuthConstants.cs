ff63d81388d47ab369f4fc66e3a6876ea5dc4e31
dishen <dishen@atliq.com>
2025-08-16 16:59:07 +0530

    ADD: app level authentication and authorization

---
+++ 3.Auth/Movies.Api/AuthConstants.cs
@@ -0,0 +1,10 @@
+namespace Movies.Api;
+
+public static class AuthConstants
+{
+    public const string AdminUserPolicyName = "Admin";
+    public const string AdminUserClaimName = "admin";
+    
+    public const string TruestMemberPolicyName = "Trusted";
+    public const string TruestMemberClaimName = "trusted_member";
+}
